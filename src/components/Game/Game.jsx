8a033ec5e20afb41c44d210d8431a0e9655823f7
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2024-01-27 17:35:51 +0100

    added state and getRandomWord function

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hangman from "../Hangman/Hangman";
 import WordDisplay from "../WordDisplay/WordDisplay";
 import Keyboard from "../Keyboard/Keyboard";
 import Hints from "../Hints/Hints";
+import { wordList } from "../../words/wordList";
 
 const Game = () => {
+  const [data, setData] = useState({
+    word: "",
+    hint: "",
+    win: false,
+    pressedLetters: [],
+    missingLetters: [],
+  });
+
+  const getRandomWord = () => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    setData({ word: word, hint: hint });
+  };
+
+  useEffect(() => {
+    getRandomWord();
+  }, []);
+
   return (
     <div className="container">
       <div>
