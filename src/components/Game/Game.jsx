605dd490d4ee5821f9bc8bbcadf335c6e14f996e
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2024-01-27 15:59:39 +0100

    added hints component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Hangman from "../Hangman/Hangman";
+import WordDisplay from "../WordDisplay/WordDisplay";
+import Keyboard from "../Keyboard/Keyboard";
+import Hints from "../Hints/Hints";
 
 const Game = () => {
   return <div></div>;
---
+++ src/components/Hints/Hints.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hints = () => {
+  return <div></div>;
+};
+
+export default Hints;
